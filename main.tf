eede0441e987f7b987f3bbfcfe8d1c983dca309e
Mrudul Lole <mrudullole08@gmail.com>
2026-05-03 22:43:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
-d
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "devops-vm"
+  resource_group_name = "your-rg-name"
+  location            = "Central India"
+  size                = "Standard_B1s"
+
+  admin_username = "azureuser"
+
+  network_interface_ids = [
+    "/subscriptions/<SUBSCRIPTION_ID>/resourceGroups/<RG_NAME>/providers/Microsoft.Network/networkInterfaces/<NIC_NAME>"
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
